0a48b81cba27720cbca90ced5d6d9d62b74a8934
cukhoaito <ngobac20031016@gmail.com>
2024-10-16 00:07:11 +0700

    update

--- src/BuildingBlocks/BuildingBlocks/Models/Pagination/PaginatedResult.cs
+++ src/BuildingBlocks/BuildingBlocks/Models/Pagination/PaginatedResult.cs
@@ -5,10 +5,6 @@ public class PaginatedResult<TEntity>
 {
     public IEnumerable<TEntity> Data { get; } = data;
     public int PageIndex { get; } = pageIndex;
-    public int TotalPages => (int)Math.Ceiling((double)Count / PageSize);
     public long Count { get; } = count;
     public int PageSize { get; } = pageSize;
-
-    public bool HasPreviousPage => PageIndex > 1;
-    public bool HasNextPage => PageIndex < TotalPages;
 }
